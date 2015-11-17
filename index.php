cd5efbd5b83f7ea256de97cd9c23d63a102211d9
itdima <itdima@mail.ru>
2015-11-17 16:25:52 +0400

    Изменена иконка и .gitignore

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
-require(__DIR__ . '/../familialvalues/vendor/autoload.php');
-require(__DIR__ . '/../familialvalues/vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/project/vendor/autoload.php');
+require(__DIR__ . '/project/vendor/yiisoft/yii2/Yii.php');
 
-$config = require(__DIR__ . '/../familialvalues/config/web.php');
+$config = require(__DIR__ . '/project/config/web.php');
 
 (new yii\web\Application($config))->run();
 
